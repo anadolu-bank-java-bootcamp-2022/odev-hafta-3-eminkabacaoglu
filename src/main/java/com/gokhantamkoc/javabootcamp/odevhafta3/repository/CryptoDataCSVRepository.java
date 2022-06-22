b6bcf3eadbe995c932e4d5cfaa779801bdd271fe
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-06-23 00:44:50 +0300

    done

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta3/repository/CryptoDataCSVRepository.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta3/repository/CryptoDataCSVRepository.java
@@ -1,8 +1,6 @@
 package com.gokhantamkoc.javabootcamp.odevhafta3.repository;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +15,34 @@ public class CryptoDataCSVRepository implements CSVRepository {
 		List<Candle> candles = new ArrayList<Candle>();
 		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
 		// Bu alandan itibaren kodunuzu yazabilirsiniz
-		
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+		try {
+			BufferedReader bf =new BufferedReader(inputStreamReader);
+			String line;
+			List<String> temp;
+			while ((line = bf.readLine())!=null){
+				temp = List.of(line.split(COMMA_DELIMITER));
+
+				// control for ignoring csv headers unix,date,symbol,open,high,low,close,volume,tradecount
+				if(temp.get(0).equals("unix")){
+					continue;
+				}
+				// parsing string to long and double
+				long time = Long.parseLong(temp.get(0));
+				double open = Double.parseDouble(temp.get(3));
+				double high = Double.parseDouble(temp.get(4));
+				double low = Double.parseDouble(temp.get(5));
+				double close = Double.parseDouble(temp.get(6));
+				double volume = Double.parseDouble(temp.get(7));
+
+				Candle candle = new Candle(time,open,high,low,close,volume);
+				candles.add(candle);
+
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 		// Bu alandan sonra kalan kod'a dokunmayiniz.
 		return candles;
 	}
